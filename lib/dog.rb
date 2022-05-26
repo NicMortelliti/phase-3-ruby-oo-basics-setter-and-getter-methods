c82015e0a89b82b3c7a6f7667a54efa60c587f83
NicMortelliti <npmortelliti@gmail.com>
2022-05-26 16:13:32 -0700

    Assignment complete

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,20 @@
 class Dog
+  # Setter method
+  def name=(name)
+    @name = name
+  end
 
+  # Getter method
+  def name
+    @name    
+  end
+
+  def breed=(breed)
+    @breed = breed
+  end
+
+  def breed
+    @breed
+  end
+  
 end
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,19 @@
 class Person
+  # Setter method
+  def name=(name)
+    @name = name
+  end
 
+  def name
+    @name
+  end
+
+  # Getter method
+  def job=(job)
+    @job = job
+  end
+
+  def job
+    @job
+  end
 end
